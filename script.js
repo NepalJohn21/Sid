bdb6b138df3d299b0e0992a8203470756a48df54
NepalJohn21 <124006434+NepalJohn21@users.noreply.github.com>
2023-02-18 17:11:55 -0300

    only yeah

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 localStorage.setItem("sid", 0)
+localStorage.setItem("sidpc", 1)
+const f1 = () => {
+    if (Number(localStorage.sid) >= 500) {
+        localStorage.setItem("sid", Number(localStorage.sidpc) - 500)
+        localStorage.setItem("sidpc", Number(localStorage.sidpc) + 1)
+    } else {
+        alert("Not enough this!")
+    }
+}
 
 setInterval(() => {
     document.querySelector(".sids").innerHTML = localStorage.sid
